9200e36c2c6ffc314de50091ed210d9aacb76227
Daniil Studenikin <fastrapier1@yandex.ru>
2021-03-18 00:48:20 +0300

    Codewars_kyu_7

---
+++ kyu_7/BeginnerSeries_3_SumOfNumbers.java
@@ -0,0 +1,37 @@
+public class BeginnerSeries_3_SumOfNumbers {
+    public static void main(String[] args) {
+        BeginnerSeries_3_SumOfNumbers beginnerSeries_3_sumOfNumbers = new BeginnerSeries_3_SumOfNumbers();
+        System.out.println(beginnerSeries_3_sumOfNumbers.GetSum(1, 0));
+        System.out.println(beginnerSeries_3_sumOfNumbers.GetSum(1, 2));
+    }
+
+    public int GetSum(int a, int b) {
+        int sum = 0;
+        //Good luck!
+        if (a > b) {
+            for (int i = b; i <= a; i++) {
+                sum += i;
+            }
+        } else if (b > a) {
+            for (int i = a; i <= b; i++) {
+                sum += i;
+            }
+        } else
+            return a;
+        return sum;
+    }
+
+    public int GetSum1(int a, int b) {
+        return (a + b) * (Math.abs(a - b) + 1) / 2;
+        //Не знаю что за формула, но работает  :)
+    }
+
+    public int GetSum2(int a, int b) {
+        int sum = 0;
+        for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            sum += i;
+        }
+        return a == b ? a : sum;
+    }
+    //думаю лучшее решение
+}
---
+++ kyu_7/TwoToOne.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+//TODO: Не решил
+//public class TwoToOne {
+//    public static void main(String[] args) {
+//        String a = "xyaabbbccccdefww";
+//        String b = "xxxxyyyyabklmopq";
+//        System.out.println(longest(a, b));
+//    }
+
+//    public static String longest(String s1, String s2) {
+//        // your code
+//        StringBuilder test = new StringBuilder(s1);
+//        StringBuilder test2 = new StringBuilder(s2);
+//        HashSet<String> strings = new HashSet<>();
+//        HashSet<String> strings1 = new HashSet<>();
+//        for (int i = 0; i < test.length(); i++) {
+//            strings.add(test.charAt(i) + "");
+//        }
+//        for (int i = 0; i < test2.length(); i++) {
+//            strings1.add(test2.charAt(i) + "");
+//        }
+//        String[] arr = ;
+//        return .toString();
+//    }
+//}
