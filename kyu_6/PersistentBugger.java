48b6f3e12aa916e3853c04a432fbc47c531bd9fd
Daniil Studenikin <fastrapier1@yandex.ru>
2021-04-26 00:38:11 +0300

    Codewars_kyu_6

---
+++ kyu_6/PersistentBugger.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class PersistentBugger {
+    public static void main(String[] args) {
+        System.out.println(persistence(39));
+        System.out.println(persistence(4));
+        System.out.println(persistence(25));
+        System.out.println(persistence(999));
+
+    }
+
+    public static int persistence(long n) {
+
+        int counter = 0;
+        while (n / 10 != 0) {
+            String[] nums = String.valueOf(n).split("");
+            int answer = 1;
+            for (String num : nums) {
+                answer *= Integer.parseInt(num);
+            }
+            counter++;
+            n = answer;
+
+        }
+        return counter;
+    }
+}
+
