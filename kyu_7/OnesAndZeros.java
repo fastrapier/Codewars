0b3595943b6f0a73b43c0c3096a3e1a74b67a061
Daniil Studenikin <fastrapier1@yandex.ru>
2021-04-27 00:59:40 +0300

    Codewars_kyu_7

--- kyu_7/OnesAndZeros.java
+++ kyu_7/OnesAndZeros.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class OnesAndZeros {
+    public static void main(String[] args) {
+        System.out.println(ConvertBinaryArrayToInt(new ArrayList<>(Arrays.asList(0, 0, 0, 1))));
+    }
+
+    public static int ConvertBinaryArrayToInt(List<Integer> binary) {
+        StringBuilder binaryNum = new StringBuilder();
+        for (Integer num : binary) {
+            binaryNum.append(num);
+        }
+        return Integer.parseInt(binaryNum.toString(), 2);
+    }
 }
