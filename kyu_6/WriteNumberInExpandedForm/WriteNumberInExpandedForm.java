1e8113038a49012a74b99b6da131e5d42923cd4f
Daniil Studenikin <fastrapier1@yandex.ru>
2021-04-28 05:54:16 +0300

    Codewars_kyu_6

---
+++ kyu_6/WriteNumberInExpandedForm/WriteNumberInExpandedForm.java
@@ -0,0 +1,27 @@
+package WriteNumberInExpandedForm;
+
+public class WriteNumberInExpandedForm {
+    public static void main(String[] args) {
+        System.out.println(expandedForm(74));
+        System.out.println(expandedForm(42));
+        System.out.println(expandedForm(50012));
+        System.out.println(expandedForm(9000000));
+    }
+
+    public static String expandedForm(int num) {
+        String str = String.valueOf(num);
+        StringBuilder stringBuilder = new StringBuilder();
+        int countOfZeros = str.length() - 1;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '0') {
+                stringBuilder.append(str.charAt(i));
+                    stringBuilder.append("0".repeat(countOfZeros));
+                    stringBuilder.append(" + ");
+
+            }
+            countOfZeros--;
+        }
+        stringBuilder.delete(stringBuilder.length() - 3, stringBuilder.length());
+        return String.valueOf(stringBuilder);
+    }
+}
