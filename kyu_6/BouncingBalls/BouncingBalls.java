0b577490b16834606e183caacb5762eb1de784f6
Daniil Studenikin <fastrapier1@yandex.ru>
2021-05-03 02:04:35 +0300

    Codewars_kyu_6

---
+++ kyu_6/BouncingBalls/BouncingBalls.java
@@ -0,0 +1,11 @@
+package BouncingBalls;
+
+public class BouncingBalls {
+    public static void main(String[] args) {
+    //TODO:Сделать
+    }
+    public static int bouncingBall(double h, double bounce, double window) {
+        // your code
+        return 0;
+    }
+}
